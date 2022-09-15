c530a18fe72561a992e7c3072c84afc017c40710
fenandohspl <fernandohsp25@gmail.com>
2022-09-14 23:59:59 -0300

    14/09

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categorias = Categoria::all();
+        return view('admin.categorias.index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.categorias.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        Categoria::create($request->all());
+        return redirect()->route('categorias.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoria $categoria)
+    {
+        return abort(404, 'Pagina nao encontrada');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categoria $categoria)
+    {
+        return view('admin.categorias.edit', compact('categoria'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        $params = $request->all();
+        $categoria->update($params);
+        return redirect()->route('categorias.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoria $categoria)
+    {
+        //
+        $categoria->delete();
+        return redirect()->route('categorias.index');
+    }
+}
---
+++ resources/views/admin/categorias/index.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.main')
+
+@section('conteudo')
+
+
+    <div class="container">
+        <div class="row">
+            <a href="{{route('categorias.create')}}" class="btn btn-success">Novo</a>
+        </div>
+        <div class="row">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>-</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($categorias as $categoria)
+                    <tr>
+                        <td class="table-column-70">{{$categoria->nome}}</td>
+                        <td>
+                            <a href="{{route('categorias.edit', $categoria)}}" class="btn btn-primary">Editar</a>
+                            <form action="{{route('categorias.destroy', $categoria)}}" method="post" class="form-delete">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger" type="submit">Excluir</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
